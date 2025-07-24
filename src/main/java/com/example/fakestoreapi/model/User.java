15172fe0b8e07c924c77832753a8bbfda908315d
jaabless <jefferyasamoahbless@gmail.com>
2025-07-24 15:37:09 +0000

    Add User model class with nested Name, Address, and Geo classes

--- src/main/java/com/example/fakestoreapi/model/User.java
+++ src/main/java/com/example/fakestoreapi/model/User.java
@@ -10,6 +10,7 @@ public class User {
 
     public User() {}
 
+
     public User(int id, String email, String username, String password, Name name, Address address) {
         this.id = id;
         this.email = email;
